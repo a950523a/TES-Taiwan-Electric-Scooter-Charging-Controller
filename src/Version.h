649622dcabe7f948df8a955c5505e450a44fc939
Chris Huang <102665605+a950523a@users.noreply.github.com>
2025-09-02 23:56:22 +0800

    OTA test

--- src/Version.h
+++ src/Version.h
@@ -3,7 +3,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define FIRMWARE_VERSION "v2.2.0-dev2"
-#define FILESYSTEM_VERSION "v1.0.0"
+#define FIRMWARE_VERSION "v2.2.0-dev3"
+#define FILESYSTEM_VERSION "v1.0.0-dev1"
 
 #endif // VERSION_H
