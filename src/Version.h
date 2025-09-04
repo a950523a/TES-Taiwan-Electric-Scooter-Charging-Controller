3760b3ffa03a1648d290097f6ee3bb150b507872
Chris Huang <102665605+a950523a@users.noreply.github.com>
2025-09-04 17:18:22 +0800

    v2.2.1-dev1
    Improve wifi logic and web UI

--- src/Version.h
+++ src/Version.h
@@ -3,12 +3,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-<<<<<<< Updated upstream
-#define FIRMWARE_VERSION "v2.2.0-dev4"
-#define FILESYSTEM_VERSION "v1.0.0-dev1"
-=======
 #define FIRMWARE_VERSION "v2.2.1-dev1"
-#define FILESYSTEM_VERSION "v1.0.0-dev2"
->>>>>>> Stashed changes
+#define FILESYSTEM_VERSION "v1.1.0-dev1"
 
 #endif // VERSION_H
